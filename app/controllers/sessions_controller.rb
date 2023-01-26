5af9db032c541a3e0f938bd21eb8deb67333fa21
Hunter Ogden <hunter.ogden@workwave.com>
2023-01-25 23:06:47 -0500

    lots of changes: adding messages, controllers, lots of stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
+  before_action :loggin_in_redirect, only: [:new, :create]
 
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
-      redirect_to user
+      redirect_to root_path
+      flash[:success] = "Logged in successfully"
     else
-      flash.now[:alert] = "There was something wrong with your login details"
+      flash.now[:error] = "There was something wrong with your login"
       render 'new'
     end
   end
@@ -18,7 +19,15 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged out"
-    redirect_to root_path
+    redirect_to login_path
   end
 
+  private
+
+  def loggin_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
 end
